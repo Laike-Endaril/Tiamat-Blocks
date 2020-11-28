34e01e23f1f0c1550b191390010b3d6517c6a17b
Kyle Koder <silvertallon@gmail.com>
2020-11-28 15:10:22 -0800

    1.12.2.000e

--- src/main/java/com/fantasticsource/tiamatblocks/TiamatBlocks.java
+++ src/main/java/com/fantasticsource/tiamatblocks/TiamatBlocks.java
@@ -18,7 +18,7 @@ public class TiamatBlocks
 {
     public static final String MODID = "tiamatblocks";
     public static final String NAME = "Tiamat Blocks";
-    public static final String VERSION = "1.12.2.000d";
+    public static final String VERSION = "1.12.2.000e";
 
     @Mod.EventHandler
     public static void prePreInit(FMLConstructionEvent event)
